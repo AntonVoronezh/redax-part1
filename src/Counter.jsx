521353816e7f7f9a39c55866ff8434e5f339fbd1
AntonVoronezh <pstut@mail.ru>
2019-04-02 16:17:10 +0300

    7#3

---
+++ src/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {connect} from 'react-redux'
+
+class Counter extends Component {
+    render() {
+        return (
+            <div style={{padding:20, border:'1px solid red'}}>
+            <h1>Counter {this.props.counter}</h1>
+            <hr/>
+            <div>
+                <button onClick={()=> this.props.onChange(1)}>add</button>
+                <button onClick={()=> this.props.onChange(-1)}>sub</button>
+            </div>
+            </div>
+            
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        counter:state.counter2.counter2
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+      onChange: number => dispatch({type: 'ADD2', payload: number}),
+      
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
