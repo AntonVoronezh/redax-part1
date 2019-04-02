322a47fe1f3c1bda1178d06e0a0192b0b88517ba
AntonVoronezh <pstut@mail.ru>
2019-04-02 16:38:39 +0300

    8

--- src/index.js
+++ src/index.js
@@ -3,15 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import {
+    createStore, applyMiddleware
+} from 'redux';
+import {
+    Provider
+} from 'react-redux';
 import rootReducer from './redux/rootReducer'
 
-const store = createStore(rootReducer);
+// function loggerMiddleware(store) {
+//     return function (next) {
+//         return function (action) {
+//             const result = next(action);
+//             console.log('Middleware', store.getState());
+            
+//             return result;
+//         }
+//     } 
+// }
 
-const app = (
-    <Provider store={store}>
-        <App />
+const loggerMiddleware = store => next => action => {
+    const result = next(action);
+    console.log('Middleware', store.getState());
+    return result;
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+const app = ( <Provider store = {store} >
+    <App />
     </Provider>
 );
 
