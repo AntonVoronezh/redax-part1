eadf194db1d221ab7fe317872a571020ce0032e5
AntonVoronezh <pstut@mail.ru>
2019-04-02 16:16:51 +0300

    7@2

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,25 +1,8 @@
-const initialState = {
-    counter: 0
-};
+import {combineReducers} from 'redux'
+import counter1 from '../redux/reducers/counter1';
+import counter2 from '../redux/reducers/counter2';
 
-export default function rootReducer(state = initialState, action) {
-
-    switch (action.type) {
-        case 'ADD':
-            return {
-                counter: state.counter + 1
-            }
-        case 'SUB':
-            return {
-                counter: state.counter -1
-            }
-        case 'ADD_NUMBER':
-            return {
-                counter: state.counter  + action.payload
-            }
-        default:
-            return state
-    }
-
-    // return state;
-}
+export default combineReducers({
+    counter1,
+    counter2
+})
